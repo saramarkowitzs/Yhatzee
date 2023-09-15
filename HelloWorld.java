6be8f02660ac3066386c61c486088e402600221d
Sara Markowitz <sara.markowitz@mrjaffesclass.com>
2023-09-15 11:19:03 -0700

    aewfd

---
+++ HelloWorld.java
@@ -0,0 +1,21 @@
+public class HelloWorld
+{
+
+    int a = 5;
+    int b = 2;
+    int c = 1;
+    int d = -8;
+    
+    public void run() {
+        System.out.println("Hello World!");
+    }
+    
+    
+    public static void main(String[] args)
+    {        
+        HelloWorld hw = new HelloWorld();
+        hw.run();
+    }
+}
+
+

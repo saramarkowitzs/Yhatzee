2bf834bd6c3981b9543d72325c09c266b5e0575b
Sara Markowitz <sara.markowitz@mrjaffesclass.com>
2023-09-15 11:43:19 -0700

    adfho

--- HelloWorld.java
+++ HelloWorld.java
@@ -7,24 +7,22 @@ public class HelloWorld
     int d = -8;
     
     public void run() {
-    if (a==5){
+        System.out.println("Hello World!");if (a==5){
         System.out.println("SUCSESS");
     }
     
     if (b==2){
         System.out.println("SUCSESS");
     } else {
-         System.out.println("FALIURE");
+        System.out.println("FALIURE");
+        }
     }
-    }
-    
-    
+
     public static void main(String[] args)
     {        
         HelloWorld hw = new HelloWorld();
         hw.run();
     }
 }
-
 
 
